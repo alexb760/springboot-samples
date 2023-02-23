e40210c43ca803b009932cb5b055367e1cec9b83
Jhon Alexander Bravo Martinez <abravo@abravo-mac.local>
2023-02-23 16:58:52 -0500

    first commit

---
+++ movi-proccesor/src/main/java/com/example/integration/moviproccesor/MoviProccesorApplication.java
@@ -0,0 +1,13 @@
+package com.example.integration.moviproccesor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviProccesorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviProccesorApplication.class, args);
+	}
+
+}
---
+++ movi-proccesor/src/test/java/com/example/integration/moviproccesor/MoviProccesorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integration.moviproccesor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviProccesorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
