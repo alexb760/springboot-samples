1fddf13c492321382195ce1b26f7ec41b3eb6dc2
Jhon Alexander Bravo Martinez <abravo@abravo-mac.local>
2023-02-24 18:45:18 -0500

    creating component to transfor input channel into a java POJO

--- movi-proccesor/src/main/java/com/example/integration/moviproccesor/core/MovieEndpoint.java
+++ movi-proccesor/src/main/java/com/example/integration/moviproccesor/core/MovieEndpoint.java
@@ -1,5 +1,6 @@
 package com.example.integration.moviproccesor.core;
 
+import com.example.integration.moviproccesor.core.service.MovieService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.integration.annotation.MessageEndpoint;
@@ -9,18 +10,23 @@ import org.springframework.util.StreamUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
 
 @MessageEndpoint
 public class MovieEndpoint {
+
+    private final MovieService movieService;
+    public MovieEndpoint(MovieService movieService) {
+        this.movieService = movieService;
+    }
     private static final Logger log = LoggerFactory.getLogger(MovieEndpoint.class);
     @ServiceActivator
     public void process(File input, @Headers Map<String, Objects> headers) throws IOException {
         FileInputStream inputStream = new FileInputStream(input);
-        String movie = new String(StreamUtils.copyToByteArray(inputStream));
+//        String movie = new String(StreamUtils.copyToByteArray(inputStream));
+        String movie = movieService.formatMovieFromChannel(new String(StreamUtils.copyToByteArray(inputStream)));
         inputStream.close();
         log.info(movie);
     }
---
+++ movi-proccesor/src/main/java/com/example/integration/moviproccesor/core/service/MovieService.java
@@ -0,0 +1,44 @@
+package com.example.integration.moviproccesor.core.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+  By adding this annotation we will make it visible from the Spring container so that you will see
+  in the activator service.
+ */
+
+@Component
+public class MovieService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieService.class);
+
+    public String formatMovieFromChannel(String contents){
+        LOGGER.info("Formating to Json...");
+        String json = "{}";
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            json = mapper.writeValueAsString(parseContent(contents));
+            LOGGER.info(json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return json;
+    }
+
+    private List<Movie> parseContent(String contents) {
+        List<Movie> movies = new ArrayList<>();
+        String[] record = null;
+        for (String line : contents.split(System.getProperty("line.separator"))){
+            record = Arrays.stream(line.split(",")).map(String::trim).toArray(String[]::new);
+            movies.add(new Movie(record[0],record[1], Integer.valueOf(record[2])));
+        }
+        return movies;
+    }
+}
